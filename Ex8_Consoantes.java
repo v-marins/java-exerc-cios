2e5371ba00e35406fe9fb082fa11e407bf05b85f
Vinicius Marins <vmarins.dev@gmail.com>
2021-11-30 02:50:58 -0300

    Adicionado novos Exercicios

---
+++ Ex8_Consoantes.java
@@ -0,0 +1,46 @@
+package br.com.dio.exercicios;
+
+import java.util.Scanner;
+
+/*Faça um programa que leia um vetor de 6 caracteres e diga quantas consoantes
+ * foram lidas, imprima as consoantes.*/
+
+public class Ex8_Consoantes {
+
+	public static void main(String[] args) {
+
+		try (Scanner scan = new Scanner(System.in)) {
+			String[] consoantes = new String[6];
+			int qtdConsoantes = 0;
+			int count = 0;
+
+			do {
+
+				System.out.println("Digite somente uma letra: ");
+				String letra = scan.next();
+
+				if (!(letra.equalsIgnoreCase("a") || letra.equalsIgnoreCase("e") || letra.equalsIgnoreCase("i")
+						|| letra.equalsIgnoreCase("o") || letra.equalsIgnoreCase("u"))) {
+
+					consoantes[count] = letra; // add a letra digitada ao array.
+					qtdConsoantes++;
+
+				}
+
+				count++;
+
+			} while (count < consoantes.length);
+
+			System.out.println("Consoantes: ");
+			for (String consoante : consoantes) {
+
+				if (consoante != null)
+					System.out.print(consoante + " ");
+			}
+
+			System.out.println("\n"+"Quantidade de Consoantes: " + qtdConsoantes);
+		}
+
+	}
+
+}
---
+++ Ex9_NumerosAleatorios.java
@@ -0,0 +1,33 @@
+package br.com.dio.exercicios;
+
+import java.util.Random;
+
+/*
+ *Faça um programa que leia 20 numeros inteiros aleatorios, entre 0 e 100 e armazene em um vetor.
+ *Ao final, mostre os numeros e seus sucessores. */
+
+public class Ex9_NumerosAleatorios {
+	public static void main(String[] args) {
+
+		Random random = new Random();
+
+		int[] numAleatorios = new int[20];
+
+		for (int i = 0; i < numAleatorios.length; i++) {
+			int numeros = random.nextInt(100);
+			numAleatorios[i] = numeros;
+		}
+
+		System.out.println("Numeros Aleatórios: ");
+		for (int i : numAleatorios) {
+			System.out.print((i) + " ");
+		}
+
+		System.out.println("\n" + "Sucessores numeros aleatórios: ");
+		for (int i : numAleatorios) {
+			System.out.print((i + 1) + " ");
+		}
+
+	}
+
+}
